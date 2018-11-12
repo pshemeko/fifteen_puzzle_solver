eaba4ca2a55fbfbdaa8d991605ecb4253ccd2298
przemo <p.otrembski@wp.pl>
2018-11-12 22:27:48 +0100

    changed program now is taking program parameter!!!!!!
    program now need to by started with parameters

--- fifteen_puzzle_solver/src/Contex.cpp
+++ fifteen_puzzle_solver/src/Contex.cpp
@@ -3,7 +3,7 @@
 #include <numeric>
 #include <vector>
 #include <Puzzle.h>
-
+//#include "Exception_wrong_move.h"
 
 Contex::Contex(puzzleDataType size_x, puzzleDataType size_y, std::vector< puzzleDataType> setup)
 	:sizeX{size_x}, sizeY{size_y}
@@ -37,28 +37,37 @@ auto Contex::set(char* name) -> void
 {
     std::ifstream file;
 	
-
     file.open(name);
+	if (file.is_open())
+		{
 
-    file >> sizeX;
-    file >> sizeY;
-	int a;
-	std::vector<puzzleDataType> data;
-	std::shared_ptr<Puzzle> wsk;
-    for (size_t i = 0; i < sizeX*sizeY; ++i)
-    {
-		file >> a;
-		data.push_back(a);
+			file >> sizeX;
+			file >> sizeY;
+			int a;
+			std::vector<puzzleDataType> data;
+			std::shared_ptr<Puzzle> wsk;
+			for (size_t i = 0; i < sizeX*sizeY; ++i)
+			{
+				file >> a;
+				data.push_back(a);
+
+				//TODO teraz robie
+
+				//ale sutasdfa
+			}
+			Puzzle pp = Puzzle(sizeX, sizeY, data);
+
+			wsk = std::make_shared<Puzzle>(pp);
+			swap(start, wsk);
+
+		}
+	else { 
+		//throw new Exception_wrong_move("Nie udalo sie otworzyc pliku"); 
+		std::string ss = name;
 		
-        //TODO teraz robie
-
-        //ale sutasdfa
-    }
-	Puzzle pp = Puzzle(sizeX, sizeY, data);
-
-	 wsk = std::make_shared<Puzzle>(pp);
-	swap(start, wsk);
-
+		throw "Plik o nazwie " + ss + " nie istnieje!!!!!!!!!!!!!!!";
+		std::abort();
+	}
 	//std::cout << std::endl <<" wielkosc tablicy data:"<< data.size() << std::endl;
 	//for (size_t i = 0; i < data.size(); ++i)
 	//{
--- fifteen_puzzle_solver/src/MethodBFS.cpp
+++ fifteen_puzzle_solver/src/MethodBFS.cpp
@@ -41,8 +41,13 @@ auto MethodBFS::run(Solution &solution) -> void
 	//Puzzle puzelKoncowy = *startPuzzel.get();
 	std::shared_ptr<Node> wezelKoncowy = root;
 
-	while (stillRun)
+	while (stillRun) // potem while(!frontier.empty()) // i wywalic to oganiczenie rekursjii
 	{
+		father = frontier.front();
+		explored.push_back(father);
+		frontier.pop_front();
+
+
 		//std::cout << "\t\t\t\t\t\t\t\t\t\tstill run: " << stillRun << std::endl;
 		if (MAXIMUM_PERMITTED_RECURSION_DEPTH == ile)
 		{
@@ -55,12 +60,12 @@ auto MethodBFS::run(Solution &solution) -> void
 			
 			Puzzle puzelek = *father->puzel.get();
 			//std::shared_ptr<Puzzle> puz = father->puzel;
-			bool zmienil = puzelek.MoveZero(mov);		//TODO tu trzeba przerwac bo nie mozna zrobic ruchu
+			//bool zmienil = puzelek.MoveZero(mov);		//TODO tu trzeba przerwac bo nie mozna zrobic ruchu
 			
-			if (zmienil)
+			if (puzelek.MoveZero(mov))
 				{
 
-					cout << "czy zmienil plozenie zera " << zmienil << " z ruchem" << mov;
+					//cout << "czy zmienil plozenie zera " << zmienil << " z ruchem" << mov;
 					//cout << puzelek;
 					std::shared_ptr<Puzzle> kopia = std::make_shared<Puzzle>(puzelek);
 
@@ -81,25 +86,8 @@ auto MethodBFS::run(Solution &solution) -> void
 					//////////////////////////TO DO  sprawdz czy dziala break jak docelowy to koniec moze lepiej goto;
 					bool czyJuzJest = false;
 
-
-					for (auto x : frontier)
-					{
-						//Node d = *x.get(); 
-						//Puzzle pp = *x->puzel; 
-
-						if ((x->puzel->hasHFunction()) == nod->puzel->hasHFunction())
-						{
-							czyJuzJest = true;
-							//if (  *nod.get() == *x.get()) czyJuzJest = true;
-							std::cout << "\n\t\t\t\tjest juz w frontier:" << nod->puzel->hasHFunction() << "ruch byl:" << mov << std::endl;
-								break;
-						}
-					}
 					for (auto x : explored)
 					{
-						//Node d = *x.get(); 
-						//Puzzle pp = *x->puzel; 
-
 						if ((x->puzel->hasHFunction()) == nod->puzel->hasHFunction())
 						{
 							czyJuzJest = true;
@@ -107,6 +95,22 @@ auto MethodBFS::run(Solution &solution) -> void
 							std::cout << "\n\t\t\t\tjest juz w explored:" << nod->puzel->hasHFunction() << "ruch byl:" << mov << std::endl;
 							break;
 						}
+					}
+
+					if (!czyJuzJest)
+					{
+
+						for (auto x : frontier)
+						{
+							if ((x->puzel->hasHFunction()) == nod->puzel->hasHFunction())
+							{
+								czyJuzJest = true;
+								//if (  *nod.get() == *x.get()) czyJuzJest = true;
+								std::cout << "\n\t\t\t\tjest juz w frontier:" << nod->puzel->hasHFunction() << "ruch byl:" << mov << std::endl;
+								break;
+							}
+						}
+
 					}
 
 					if (!czyJuzJest)
@@ -120,12 +124,15 @@ auto MethodBFS::run(Solution &solution) -> void
 
 			}
 
+			if (!stillRun ) break; // przenosze braeak dalej
 		}
 		//teraz biore pierwszy i przetwarzam
 		
-		father = frontier.front();
-		explored.push_back(father);
-		frontier.pop_front();
+		//father = frontier.front();
+		//explored.push_back(father);
+		//frontier.pop_front();
+
+
 		if(stillRun) ile++;///// czy ok 
 		std::cout << "\n koniec for:" << ile;
 	}
@@ -136,19 +143,18 @@ auto MethodBFS::run(Solution &solution) -> void
 
 	//******************************** WYSWIETLANIE 
 
-	//TODO zle wypisuje operatory brakuje jednego i sa pomieszane
-
-	std::list<Moves> listMovesRevert;
+	
+	std::list<Moves> listMoves;
 	bool oneMore = true;
 	std::shared_ptr <Node> wezel = wezelKoncowy;
 	while (oneMore)
 	{
-		listMovesRevert.push_back(wezel->operatorUsed);
+		listMoves.push_back(wezel->operatorUsed);
 		//std::cout << "\noneMore:" << oneMore << " operator:" << wezel->operatorUsed;
 		wezel = wezel->parrent;
 		if (wezel->parrent == nullptr) oneMore = false;
 	}
-	cout <<  "\t\t size listrevert:" << listMovesRevert.size();
+	cout <<  "\t\t size listrevert:" << listMoves.size();
 	// zaweira w dobrej kolejnosci kolejnosc ruchow od poczatku do rozwiazania
 	
 	//std::list<Moves> listMoves;
@@ -157,40 +163,47 @@ auto MethodBFS::run(Solution &solution) -> void
 	//	listMoves.push_back(*it);
 	//}
 
-	std::reverse(std::begin(listMovesRevert), std::end(listMovesRevert)); // odwracam liste
+	std::reverse(std::begin(listMoves), std::end(listMoves)); // odwracam liste
 
 	solution.fileSolution << "glebokosc:" << ile << std::endl;
 	std::cout << "\nglebokosc:" << ile << std::endl;
 
-	for(auto x : listMovesRevert)
+	for(auto x : listMoves)
 	{
 		solution.fileSolution << x <<" ";
 		std::cout << x << " ";
 	}
 
-	std::cout << *wezelKoncowy->puzel.get();
+	//std::cout << *wezelKoncowy->puzel.get();
 	
 
 
 	// *********************************************      zbieranie wynikow
 
-
 	solution.time_duration_of_process = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::high_resolution_clock::now() - timeStart);
 
-	solution.length_of_the_solution_found = listMovesRevert.size();
+	solution.length_of_the_solution_found = listMoves.size();
 	solution.number_of_visited_states = frontier.size() + explored.size();
 	solution.number_of_processed_states = explored.size();
 	////?????????TODO czy to jest dobrze?
 	solution.maximum_depth_of_recursion_achieved = ile;
-	for (auto x : listMovesRevert)
+	for (auto x : listMoves)
 	{
 		if (x == Moves::Down)	solution.solution += "D";
 		if (x == Moves::Up)		solution.solution += "U";
 		if (x == Moves::Left)	solution.solution += "L";
 		if (x == Moves::Right)	solution.solution += "R";
 	}
-	cout << "\n Czas rozwiazania: " << solution.time_duration_of_process.count();
-	cout << "\n Dlugosc znalezionego rozwiazania w milisekundach: " << solution.length_of_the_solution_found;
+	cout << "\n\n\n\n ***************************************   podsumowanie wynikow programu";
+
+	cout << endl << endl << endl << endl<<"Puzel poczatkowy:\n";
+	std::cout << *startPuzzel << " jego hash: " << startPuzzel->hasHFunction() << std::endl;
+	cout << "\nPuzel koncowy:";
+	std::cout << *wezelKoncowy->puzel.get() << " jego hash: " << wezelKoncowy->puzel->hasHFunction() << std::endl;
+	
+
+	cout << "\n Czas rozwiazania w milisekundach: " << solution.time_duration_of_process.count();
+	cout << "\n Dlugosc znalezionego rozwiazania: " << solution.length_of_the_solution_found;
 	cout << "\n Operatory uzyte: " << solution.solution;
 	cout << "\n liczba stanow odwierdzonych:  " << solution.number_of_visited_states;
 	cout << "\n liczba stanow przetworzonych: " << solution.number_of_processed_states;
